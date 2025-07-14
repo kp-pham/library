1e6588d1cac2b6e968efb3ae0a33a3cfbe887ec7
kp-pham <170148118+kp-pham@users.noreply.github.com>
2025-07-14 15:25:34 -0700

    feat: create function to add book to library
    Construct book and append book to the array.

--- index.js
+++ index.js
@@ -10,3 +10,7 @@ function Book(title, author, pages, read) {
     this.pages = pages;
     this.read = read;
 }
+
+function addBookToLibrary(title, author, pages, read) {
+    library.push(new Book(title, author, pages, read));
+}
